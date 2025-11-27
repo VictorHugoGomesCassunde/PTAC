3a0663b96b93e6c4f76464a5a6e7e910bbf64064
Vitor (zezao) <vitor.correa@estudante.ifms.edu.br>
2025-11-26 21:24:03 -0400

    mexer cardapio, foto ...

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,28 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './Pages/Home';
-import Login from './Pages/Login';
-import Cadastro from './Pages/Cadastro';
-import Perfil from './Pages/Perfil';
-import CadastrarMesas from './Pages/CadastrarMesas';
-import ReservarMesas from './Pages/ReservarMesas';
-import BuscarMesas from './Pages/BuscarMesas';
-import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function RoutesApp() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/perfil" element={<Perfil />} />
-        <Route path="/cadastrar" element={<CadastrarMesas />} />
-        <Route path="/reservar" element={<ReservarMesas />} />
-        <Route path="/buscar" element={<BuscarMesas />} />
-      </Routes>
-    </BrowserRouter>
-  );
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Cadastro from "./Pages/Cadastro";
+import Perfil from "./Pages/Perfil";
+import ReservarMesas from "./Pages/ReservarMesas";
+import BuscarMesas from "./Pages/BuscarMesas";
+import CadastrarMesas from "./Pages/CadastrarMesas";
+import Cardapio from "./Pages/Cardapio"; ///mexer nesse cardapioe e reservas
+
+function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/cadastro" element={<Cadastro />} />
+                <Route path="/perfil" element={<Perfil />} />
+                <Route path="/reservar" element={<ReservarMesas />} />
+                <Route path="/buscar" element={<BuscarMesas />} />
+                <Route path="/mesas/cadastrar" element={<CadastrarMesas />} />
+                <Route path="/cardapio" element={<Cardapio />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default RoutesApp;
+export default AppRoutes;
